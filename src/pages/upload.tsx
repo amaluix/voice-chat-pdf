f89eda2986beba690b6f3733d6e8c22f6fc38029
Mohit Yadav <mohit.developer21@gmail.com>
2024-10-18 00:06:00 +0400

    chore: fix pdf issue related to react pdf

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -11,7 +11,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 // Configure pdfjs worker
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 export default function PdfUploader() {
   const [pdfFile, setPdfFile] = useState<File | null>(null);
